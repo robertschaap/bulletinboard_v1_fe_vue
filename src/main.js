4e6fe732159191affef5700ce86b9e9750130ec6
robertschaap <robert.schaap@mac.com>
2018-03-10 13:25:52 +0100

    rough import

--- src/main.js
+++ src/main.js
@@ -2,6 +2,9 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 
+import './assets/css/normalize.css'
+import './assets/css/stylesheet.css'
+
 Vue.config.productionTip = false
 
 new Vue({
