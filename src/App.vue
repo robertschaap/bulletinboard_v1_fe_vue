f4fedab19ce7b9620c18b6aced755ed9bb7659cb
robertschaap <robert.schaap@mac.com>
2018-03-10 13:25:52 +0100

    rough import

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,48 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <router-view/>
+
+    <header>
+      <div class="wrapper">
+        <h1>Bulletin Board</h1>
+        <nav>
+          <a href="#">Write</a>
+          <a href="#">Read</a>
+        </nav>
+      </div>
+    </header>
+
+    <main>
+      <section id="comments">
+        <h2>Read Something</h2>
+        <select id="sortbtn">
+            <option value="desc">Newest First</option>
+            <option value="asc">Oldest First</option>
+        </select>
+
+        <div alt="" class="com_wrap wrapper">
+          <div class="com_info"><img src="./assets/logo.png" width="100%" /></div>
+          <div class="com_body">
+            <h3>Title</h3>
+            <h4>Name</h4>
+            <p>Body</p>
+          </div>
+        </div>
+        
+      </section>
+
+      <section>
+        <button id="loadcomments" class="wrapper" onClick={this.loadMoreComments}>Load More Comments</button>
+      </section>
+    </main>
+
+    <footer>
+      <div class="wrapper">
+        <p>Robert Schaap 2018</p>
+      </div>
+    </footer>
+
+    <!-- <img src="./assets/logo.png"> -->
+    <!-- <router-view/> -->
   </div>
 </template>
 
@@ -12,12 +53,5 @@ export default {
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
